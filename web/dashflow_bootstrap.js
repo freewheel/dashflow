d13c69488a28bd9aa3f12323fc9f5c4309bc3c09
Ben Cao <666516+bencao@users.noreply.github.com>
2019-06-24 15:33:55 -0400

    add a separate static demo page, so we can deploy the demo site

--- web/dashflow_bootstrap.js
+++ web/dashflow_bootstrap.js
@@ -1,5 +1,5 @@
 /* eslint-env browser */
-/* global React, ReactDOM, Utils, Components, Dashboards, io, jsyaml */
+/* global React, ReactDOM, Utils, Components, Dashboards, jsyaml */
 (function bootstrap() {
   const notifyThrottle = Utils.throttleFactory(500);
 
@@ -83,48 +83,7 @@
     },
   };
 
-  function createSocket(store) {
-    const socket = io();
-
-    function init() {
-      socket.emit("get_dashboards_config");
-
-      socket.emit("catchup");
-    }
-
-    socket.on("get_dashboards_config", function(dashboards) {
-      try {
-        store.clearErrorMessages();
-        store.updateDashboards(dashboards);
-        store.updateCurrentDashboardTitle(null);
-      } catch (err) {
-        store.addErrorMessage(err.message);
-      }
-    });
-
-    socket.on("catchup_batch", function(messages) {
-      store.appendEvents(messages);
-    });
-
-    socket.on("catchup_done", function() {
-      socket.emit("delta");
-    });
-
-    socket.on("delta", function(message) {
-      store.appendEvent(message);
-    });
-
-    socket.on("reconnect", function() {
-      store.resetEvents();
-      init();
-    });
-
-    init();
-
-    return socket;
-  }
-
-  createSocket(appStore);
+  window.appStore = appStore;
 
   ReactDOM.render(
     React.createElement(Components.App, { store: appStore }),
